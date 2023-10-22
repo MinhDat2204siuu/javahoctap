e643112c768f77f2b476228731d4508737cd0add
minhdatdepzai <dat.pm210169@sis.hust.edu.vn>
2023-10-22 15:53:53 +0700

    first commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void hello(String[] args)
+    {
+        String x="water";
+        String y="kotal";
+        String temp;
+        temp=x;
+        System.out.println("x: "+x);
+    }
+}
